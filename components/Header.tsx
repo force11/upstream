5e2e8dd618349e1574048f1103c2b0565b974453
Martin Fenner <martin@front-matter.io>
2021-11-09 13:33:51 +0100

    adding search box

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React, { createElement, Fragment, useEffect, useRef } from "react";
 import { render } from "react-dom";
 import { autocomplete } from "@algolia/autocomplete-js";
 import { Disclosure } from "@headlessui/react";
-// import { SearchIcon } from '@heroicons/react/solid'
+import { SearchIcon } from "@heroicons/react/solid";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import Image from "next/image";
@@ -37,28 +37,28 @@ export default function Navbar({ tags, tag }) {
                   </Link>
                 </div>
               </div>
-              {/* <div className="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
-                  <div className="w-full sm:max-w-xs">
-                    <label htmlFor="search" className="sr-only">
-                      Search
-                    </label>
-                    <div className="relative">
-                      <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-                        <SearchIcon
-                          className="h-5 w-5 text-gray-400"
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <input
-                        id="search"
-                        name="search"
-                        className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-blue-400 focus:border-blue-400 sm:text-sm font-sans"
-                        placeholder="Under construction..."
-                        type="search"
+              <div className="relative z-0 flex-1 px-2 flex items-center justify-center sm:absolute sm:inset-0">
+                <div className="w-full sm:max-w-xs">
+                  <label htmlFor="search" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative">
+                    <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
+                      <SearchIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
                       />
                     </div>
+                    <input
+                      id="search"
+                      name="search"
+                      className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-force-blue focus:border-force-blue sm:text-sm font-sans"
+                      placeholder="Search coming soon..."
+                      type="search"
+                    />
                   </div>
-                </div> */}
+                </div>
+              </div>
               <nav
                 className="py-6 space-x-6 font-semibold lg:block"
                 aria-label="Global"
