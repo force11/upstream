6ed016e7ed75f65f1bb6907e194506ddab0ad161
Martin Fenner <martin@front-matter.io>
2021-11-16 16:55:43 +0100

    enable comments

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -24,7 +24,7 @@ import {
 } from "../../lib/posts";
 import Byline from "../../components/Byline";
 import { sanitizeDescription, uuid2base32 } from "../../lib/helpers";
-// import DiscourseForum from "../../lib/discourse-forum.js";
+import DiscourseForum from "../../lib/discourse-forum.js";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await getAllPosts();
@@ -182,6 +182,7 @@ const Post = (props) => {
               Creative Commons Attribution 4.0 License.
             </a>
           </div>
+          <DiscourseForum post={props.post} />
         </div>
       </div>
       {props.recommendedPosts.length > 0 && (
