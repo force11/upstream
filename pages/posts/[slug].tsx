eb5d252ba640c8b7c744d7357db15d09192766f1
Martin Fenner <martin@front-matter.io>
2021-11-19 13:34:07 +0100

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -135,24 +135,6 @@ const Post = (props) => {
       <Header tags={props.tags} tag={{}} />
       <div className="container mx-4 md:mx-auto px-6 py-8 flex flex-wrap justify-center">
         <div className="w-full md:w-8/12 ">
-          {props.post.tags && (
-            <p className="text-sm font-medium uppercase font-sans mb-0 text-force-blue">
-              {props.post.tags.map((tag, index) => (
-                <>
-                  <Link
-                    key={tag.slug}
-                    href={`/categories/${tag.slug}`}
-                    passHref
-                  >
-                    <span className="border-b-0 hover:border-b hover:border-force-blue cursor-pointer">
-                      {tag.name}
-                    </span>
-                  </Link>
-                  {index + 1 < props.post.tags.length ? " · " : ""}
-                </>
-              ))}
-            </p>
-          )}
           <h1 className="mt-0 mb-2 text-force-blue">{props.post.title}</h1>
           <Byline
             authors={props.post.authors}
