654df7f99f173c8fb98f4c2034f65f3a1dce2ad4
Martin Fenner <martin@front-matter.io>
2021-11-27 13:58:35 +0000

    Update Top.tsx

--- components/Top.tsx
+++ components/Top.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 
 export default function Top({ tag }) {
   return (
-    <div className="relative h-full w-full bg-force-blue">
-      <div className="absolute inset-0 object-cover overflow-auto">
+    <div className="relative h-full w-auto md:w-full bg-force-blue">
+      <div className="absolute inset-0 overflow-auto">
         <Image
           className=""
           src={tag.feature_image}
-          alt=""
+          alt="teaser image"
+          objectFit="cover"
           width={2042}
           height={560}
         />
@@ -17,9 +18,9 @@ export default function Top({ tag }) {
           aria-hidden="true"
         />
       </div>
-      <div className="relative max-w-7xl mx-auto pt-1 md:pt-8 lg:pt-12 pb-4 px-10 lg:px-12">
+      <div className="relative max-w-7xl mx-4 md:mx-auto pt-1 md:pt-8 lg:pt-12 pb-4 px-4 md:px-10 lg:px-12">
         <h1 className="text-3xl font-bold text-white">{tag.name}</h1>
-        <p className="mt-6 text-xl font-sans text-white max-w-3xl">
+        <p className="mt-6 text-xl font-sans flex-wrap text-white max-w-3xl">
           {tag.description}
         </p>
       </div>
