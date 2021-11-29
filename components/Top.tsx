e03ab168a53c456c1a6039bf7119dc1b959841f9
Martin Fenner <martin@front-matter.io>
2021-11-29 10:59:06 +0000

    Update Top.tsx

--- components/Top.tsx
+++ components/Top.tsx
@@ -12,14 +12,10 @@ export default function Top({ tag }) {
           objectFit="cover"
           layout="fill"
         />
-        <div
-          className="absolute inset-0 bg-force-blue mix-blend-multiply"
-          aria-hidden="true"
-        />
       </div>
       <div className="relative max-w-7xl mx-4 md:mx-auto pt-1 md:pt-8 lg:pt-12 pb-4 px-4 md:px-10 lg:px-12">
-        <h1 className="text-3xl font-bold text-white">{tag.name}</h1>
-        <p className="mt-6 text-xl font-sans flex-wrap text-white max-w-3xl">
+        <h1 className="text-3xl font-bold">{tag.name}</h1>
+        <p className="mt-6 text-xl font-sans flex-wrap max-w-3xl">
           {tag.description}
         </p>
       </div>
