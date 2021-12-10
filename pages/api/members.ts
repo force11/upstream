f1a8909ae54d48a6aecc63e3eb0dd248ac19c96c
Martin Fenner <martin@front-matter.io>
2021-12-10 19:49:37 +0100

    Update members.ts

---
+++ pages/api/members.ts
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+
+  res.status(200).json("ok");
+}
