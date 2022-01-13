301ca2a44d74ccf4ca8d15d42389e62979946003
Martin Fenner <martin@front-matter.io>
2022-01-13 22:20:50 +0000

    Update Author.tsx

--- components/Author.tsx
+++ components/Author.tsx
@@ -491,7 +491,7 @@ export default function Author({ posts, author, pagination }) {
                   total pages for {pagination.total} posts.
                 </p>
               </div>
-              <div className="flex-1 flex justify-between sm:justify-end">
+              <div className="flex-1 flex justify-between sm:justify-end space-x-1">
                 {pagination.prev && (
                   <button onClick={() => setPageIndex(pagination.prev)}>
                     <a className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 hover:text-green-500 hover:border-green-500 hover:bg-gray-50">
